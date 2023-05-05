d7849d6b624eade01023c46b477882467a38ff5b
RichardKaranuMbuti <officialforrichardk@gmail.com>
2023-05-05 04:11:00 +0300

    initial set up files and folders

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome......</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+  </div>
+  <button @click.alt="toggleModal">Open Model (alt)</button>
+  
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+
+export default {
+  components: { Modal },
+  name: 'App',
+  data(){
+    return{
+      title:'My first Vue App title',
+      header:"sign up for the give away",
+      text:"Grab your ninja swag for half the price",
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal=!this.showModal
+    }
+  }
+
+}
+  
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}" >
+            <h1>{{ header }}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export  default{
+    props: ['header','text','theme'],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+
+}
+
+</script>
+
+<style>
+
+.modal{
+    width:400px;
+    padding: 20px;
+    margin:100px auto;
+    background: white;
+    border-radius:100px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal.sale{
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1{
+    color:white
+}
+</style>
